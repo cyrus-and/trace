f8823b797ea1bc7363f3476a87a393bfa044c27d
Andrea Cardaci <cyrus.and@gmail.com>
2014-11-19 02:09:45 +0100

    Add initial working release

---
+++ trace.default.sh
@@ -0,0 +1,29 @@
+# This is the standard metric group, some most common system metrics are defined
+# here. This file is loaded by default. Metric functions should be simple and
+# lightweight shell commands and under no circumstances they should fork
+# processes or introduce delay. Metric names should be self-descriptive and
+# should include its unit of measure when not clear by the name itself.
+
+read_b() { sed -n 's/^read_bytes: //p' /proc/$pid/io; }
+
+write_b() { sed -n 's/^write_bytes: //p' /proc/$pid/io; }
+
+buf_read_b() { sed -n 's/^rchar: //p' /proc/$pid/io; }
+
+buf_write_b() { sed -n 's/^wchar: //p' /proc/$pid/io; }
+
+core_count() { nproc; }
+
+cpu_load() { command ps -p $pid -o pcpu=; }
+
+virt_mem_kb() { command ps -p $pid -o vsz=; }
+
+res_mem_kb() { command ps -p $pid -o rss=; }
+
+swap_mem_kb() { awk '/^Swap:/ {sum+=$2} END {print sum}' /proc/$pid/smaps; }
+
+total_mem_kb() { awk '/^MemTotal:/ {print $2}' /proc/meminfo; }
+
+used_swap_kb() { awk '/^SwapCached:/ {print $2}' /proc/meminfo; }
+
+total_swap_kb() { awk '/^SwapTotal:/ {print $2}' /proc/meminfo; }
---
+++ trace.sh
@@ -0,0 +1,150 @@
+#!/usr/bin/env bash
+
+DEFAULT_GROUP='./trace.default.sh'
+
+labels=()
+metrics=()
+
+function usage {
+    echo >&2 "$1
+
+Usage:
+    trace [<options>] -- run <program> [<arguments>]
+    trace [<options>] -- attach <pid>
+
+    <options> can be a combination of:
+
+        -d                : load the default group
+        -g <group>        : group file to source
+        -m <metric>       : metric to use
+        -x <label>:<code> : custom shell code to execute in which the variable
+                            \$pid is defined to be the PID of the process being
+                            traced; the given label will be shown as column
+                            header
+        -s <separator>    : column separator (defaults to tab)
+        -i <interval>     : seconds between two consecutive measures (defaults to 1s)
+
+    Options -g, -m and -x can (and should) be used multiple times. Options are
+    evaluated in same the order as they appear."
+    exit 1
+}
+
+function waitpid {
+    # if the pid is not child of this shell then fallback to polling
+    if ! wait "$1" &> /dev/null; then
+        while kill -0 "$1" &> /dev/null; do
+            sleep 0.5
+        done
+    fi
+}
+
+function tracer {
+    local pid="$1"
+    local separator="$2"
+    local interval="$3"
+    # record time and write the first label
+    local start="$(date +%s)"
+    echo -n 'seconds'
+    # write the other custom labels
+    for label in "${labels[@]}"; do
+        echo -n -e "$separator$label"
+    done
+    echo
+    # tracer loop
+    while [ -d "/proc/$pid/" ]; do
+        # dump the time delta
+        echo -n "$(($(date +%s) - $start))"
+        # dump the next metric
+        for metric in "${metrics[@]}"; do
+            echo -n -e "$separator"
+            eval "$metric" | xargs echo -n # trim whitespaces
+        done
+        echo
+        sleep "$interval"
+    done
+}
+
+# parse common options
+OPTERR=0
+while getopts ':dg:m:x:s:i:' arg; do
+    case "$arg" in
+        'd')
+            source "$DEFAULT_GROUP"
+            ;;
+        'g')
+            source "$OPTARG"
+            ;;
+        'm')
+            # only accept aliases or functions defined in this as metrics
+            case $(type -t "$OPTARG") in
+                function|alias)
+                    labels+=("$OPTARG")
+                    metrics+=("$OPTARG")
+                    ;;
+                *)
+                    echo "'$OPTARG' must be a function or an alias defined in some group."
+                    exit 1
+            esac
+            ;;
+        'x')
+            IFS=':' read label code <<< "$OPTARG"
+            if [ -n "$label" && -n "$code" ]; then
+                labels+=("$label")
+                metrics+=("$code")
+            else
+                usage "Invalid expression format '$OPTARG'; expecting '<label>:<code>'."
+            fi
+            ;;
+        's')
+            separator="$OPTARG"
+            ;;
+        'i')
+            interval="$OPTARG"
+            ;;
+        ':')
+            usage "The argument is needed for option '$OPTARG'."
+            ;;
+        '?')
+            usage "Unknown option '$OPTARG'."
+            ;;
+    esac
+done
+
+# consume the parsed options
+shift $((OPTIND-1))
+
+# check arguments count
+if [ "$#" -lt 2 ]; then
+    usage "Missing command, either 'run' or 'attach'."
+fi
+
+# parse the command
+command="$1"
+shift
+case "$command" in
+    'run')
+        # run the program in background redirecting stdout on stderr
+        program="$1"; shift
+        "$program" "$@" >&2 &
+        program_pid="$!"
+        ;;
+    'attach')
+        program_pid="$1"
+        ;;
+    *)
+        usage "Invalid command '$command'."
+        ;;
+esac
+
+# kill all the children on ^C (SIGPIPE avoid Bash process controll feedback)
+trap 'kill -PIPE 0' SIGINT
+
+# start data collector
+tracer "$program_pid" "${separator:-\t}" "${interval:-1}" &
+tracer_pid="$!"
+
+# wait program termination and do cleanup
+waitpid "$program_pid"
+status="$?"
+kill "$tracer_pid" &> /dev/null
+exit "$status"
